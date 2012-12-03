533820eb0dc8191cf8f92fb5f3909556afcb2846
OpenShift guest <thermatk@thermatk.com>
2012-12-03 11:49:20 -0500

    Fixed encoding, working well

--- PrefixTree.java
+++ PrefixTree.java
@@ -1,38 +1,32 @@
-public class PrefixTree
-{
+public class PrefixTree {
     private char character;
-    private PrefixTree left;
-    private PrefixTree right;
+	private PrefixTree left;
+	private PrefixTree right;
 
-    public PrefixTree ()//
-    {
-        character = CharStdIn.readChar();
-        if (character == '*')
-        {
-            left = new PrefixTree ();
-            right = new PrefixTree ();
-        }
-        else return;
-    }
+	public PrefixTree() {
+		character = CharStdIn.readChar();
+		if (character == '*') {
+			left = new PrefixTree();
+			right = new PrefixTree();
+		} else {
+			
+		}
+	}
 
-    public void preorder ()
-    {
-        int k = 0;
-        if (character == '*')
-        {
-            left.preorder();
-            right.preorder();
-            k++;
-        }
-        else
-        {
-            System.out.print(character+" ");
-            System.out.println(k);
-        }
+	public void preorder(String encoding) {
+		if (character == '*') {
+			left.preorder(encoding + "0");
+			right.preorder(encoding + "1");
+			
+		} else {
+			System.out.print(character + " ");
+			System.out.println(encoding);
+		}
 
-    }
-      public static void main(String[] args)
-      {          PrefixTree tree = new PrefixTree();
-          tree.preorder();
-      }
+	}
+
+	public static void main(String[] args) {
+		PrefixTree tree = new PrefixTree();
+		tree.preorder("");
+	}
 }
