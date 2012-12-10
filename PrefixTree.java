ca5162029f38bc7a1c6edb06d0f00802134e8817
OpenShift guest <thermatk@thermatk.com>
2012-12-10 05:40:55 -0500

    New version test with Senya

--- PrefixTree.java
+++ PrefixTree.java
@@ -1,3 +1,4 @@
+
 public class PrefixTree {
     private char character;
 	private PrefixTree left;
@@ -5,11 +6,12 @@ public class PrefixTree {
 
 	public PrefixTree() {
 		character = CharStdIn.readChar();
-		if (character == '*') {
-			left = new PrefixTree();
-			right = new PrefixTree();
-		} else {
-			
+		if (!CharStdIn.isEmpty()) {
+			if (character == '*') {
+				left = new PrefixTree();
+				right = new PrefixTree();
+			} else {
+			}
 		}
 	}
 
@@ -17,16 +19,60 @@ public class PrefixTree {
 		if (character == '*') {
 			left.preorder(encoding + "0");
 			right.preorder(encoding + "1");
-			
+
 		} else {
-            //TODO: max format numbers
-    		System.out.printf("%16s%16d%16s\n",character,encoding.length(),encoding);
+			// TODO: max format numbers
+			System.out.printf("%16s%16d%16s\n", character, encoding.length(),
+					encoding);
 		}
 
+	}
+
+	public boolean isEnding() {
+		if (character == '*') {
+			return false;
+		}
+		return true;
+	}
+
+	public void uncompress() {
+		if (isEnding()) {
+			// ending
+			// System.out.println("ending");
+			System.out.print(Character.toString(character));
+			// back to root:
+			return;
+		}
+		if (!CharStdIn.isEmpty()) {
+			char currentChar = CharStdIn.readChar();
+
+			if (currentChar == '0') {
+				// System.out.println("left");
+				left.uncompress();
+				//
+
+			} else if (currentChar == '1') {
+				//
+				// System.out.println("right");
+				right.uncompress();
+
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
 		PrefixTree tree = new PrefixTree();
 		tree.preorder("");
+		//
+		// System.out.println("back to main");
+
+		// !TEST
+		CharStdIn.readChar();
+		
+		while (!CharStdIn.isEmpty()) {
+			tree.uncompress();
+		}
+		// TEST!
 	}
 }
